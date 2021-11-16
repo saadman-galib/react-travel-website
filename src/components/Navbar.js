12b8c93e40e2188625557d284718ea3465fc5371
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2021-11-16 08:54:00 +0600

    Updating files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 function Navbar() {
     const [click, setClick] = useState(false);
     const handleclick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
     return (
         <>
@@ -15,6 +16,30 @@ function Navbar() {
                 <div className="menu-icon" onClick={handleclick}>
                     <i className={click ? "fas fa-times" : "fas fa-bars"} />
                 </div>
+
+                <ul className={click ? 'nav-menu-active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+
+                </ul>
 
             </nav>
         </>
