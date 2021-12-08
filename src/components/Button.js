da8509843645fcc7a1641324f4706c6daedbd79d
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2021-12-08 22:05:46 +0600

    Updating files

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import './Button.css';
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Button.css";
+import { Link } from "react-router-dom";
 
-const STYLES = ['btn--primary', 'btn-outline']
-const SIZES = ['btn--medium', 'btn-large']
+const STYLES = ["btn--primary", "btn--outline"];
+const SIZES = ["btn--medium", "btn--large"];
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
-    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0]
-}
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+}) => {
+  let checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+  let checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <Link to="/sign-up" className="btn-mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+
+      <button>Get Started</button>
+    </Link>
+  );
+};
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
-  const [click, setClick] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
   const handleclick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  //   window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -14,7 +31,7 @@ function Navbar() {
             TRVL <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handleclick}>
-            <i className={click ? "fas fa-bars" : "fas fa-times"} />
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
 
           <ul className={click ? "nav-menu-active" : "nav-menu"}>
@@ -51,6 +68,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </>
