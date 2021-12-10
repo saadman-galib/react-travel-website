2a2542f02eb55c5f92a2d083e0cd0c59d7fed11e
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2021-12-10 20:21:26 +0600

    Updating files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        {/* <Home /> */}
         <Routes>
-          <Route path="/" exact />
+          <Route exact path="/" element={<Home />} />
         </Routes>
       </Router>
     </>
