8d5a94f96d91bcae396545012ccf3b74eb69f15b
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2021-12-12 20:18:37 +0600

    Updating files

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Navbar from "./components/Navbar";
 import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
     return (
         <>
             <Router>
                 <Navbar />
-                {/* <Home /> */}
                 <Routes>
                     <Route exact path="/" element={<Home />} />
+                    {/* <Route path="/services" element={<Services />} /> */}
+                    <Route path='/services' element={<Services />} />
+                    <Route path="/Products" element={<Products />} />
+                    <Route path="/sign-up" element={<SignUp />} />
                 </Routes>
             </Router>
         </>
---
+++ src/components/pages/Services.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.scss";
+
+function Services() {
+    return (
+        // <div >
+        // style={{ backgroundImage: `url(./images/img-2.jpg')` }}
+            <h1 className="services">
+                Services
+            </h1>
+        // </div>
+    );
+}
+
+export default Services;
